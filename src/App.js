c6f54d9b1862d732810bae527147c96d6a8e77c8
Bob Nem <bob.nem@ya.ru>
2024-08-31 19:49:17 +0300

    third commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import { useEffect } from 'react';
 import './App.css';
+import {useEffect} from "react";
 import {useTelegram} from "./hooks/useTelegram";
 import Header from "./components/header/header";
-import {Route, Routes} from 'react-router-dom';
-import ProductList from './components/productList/productList';
+import {Route, Routes} from 'react-router-dom'
+import ProductList from "./components/productList/productList";
 import Form from "./components/form/form";
 
 function App() {
-  const {onToggleButton, tg} = useTelegram();
-  useEffect(()=>{
-    tg.ready();
-  },[])
-  return (
-    <div className="App">
-      <Header />
-      <Routes>
-        <Route index element={<ProductList/>} />
-        <Route path={'/form'} element={<Form/>} />
-      </Routes>
-    </div>
-  );
+    const {onToggleButton, tg} = useTelegram();
+
+    useEffect(() => {
+        tg.ready();
+    }, [])
+
+    return (
+        <div className="App">
+            <Header />
+            <Routes>
+                <Route index element={<ProductList />}/>
+                <Route path={'form'} element={<Form />}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
